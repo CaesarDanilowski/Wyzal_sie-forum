37c35637f9c06ed2ecf259d509bbb694f415be7f
CaesarDanilowski <39009204+CaesarDanilowski@users.noreply.github.com>
2018-09-18 19:40:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function check(form)
+{
+	for (i = 0; i < form.length; i++)
+	{
+		var pole = form.elements[i];
+		if ((pole.type == "text" || pole.type == "textarea") && pole.value == "")
+		{
+			alert("Nick musi mieć od 3 do 20 znaków, tekst więcej niż 20 znaków!");
+			return false;
+		}
+	}
+	return true;
+}
+
+function load(form)
+{
+	var pole = form.elements[2];
+	
+	if (pole[0].value == "" && pole[1].value == "")
+	{
+		alert("Co najmniej jedno pole musi być wypełnione!");
+		return false;
+	}
+	
+	return true;
+}
